4d99f316a9c654d5716106c127cc4a6a47389a5b
Jose Velazquez <jav4534@nyu.edu>
2022-02-09 00:34:54 -0500

    First stab

--- solution.py
+++ solution.py
@@ -1,3 +1,4 @@
+#Author: jav4534
 # import socket module
 from socket import *
 # In order to terminate the program
@@ -9,20 +10,20 @@ def webServer(port=13331):
   #Prepare a server socket
   serverSocket.bind(("", port))
   #Fill in start
-
+  serverSocket.listen()
   #Fill in end
 
   while True:
     #Establish the connection
     #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    connectionSocket, addr = serverSocket.accept()#Fill in start      #Fill in end
     try:
 
       try:
-        message = #Fill in start    #Fill in end
+        message = "GET /helloworld.html"#Fill in start    #Fill in end
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
+        outputdata = "200 OK"#Fill in start     #Fill in end
         
         #Send one HTTP header line into socket.
         #Fill in start
@@ -38,13 +39,13 @@ def webServer(port=13331):
       except IOError:
         # Send response message for file not found (404)
         #Fill in start
-
+        outputdata = "404 Not found"
         #Fill in end
 
 
         #Close client socket
         #Fill in start
-
+        connectionSocket.close()
         #Fill in end
 
     except (ConnectionResetError, BrokenPipeError):
