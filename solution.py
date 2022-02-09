5db46f21bc1ef91cf2f21dcdaf8e34b6e4c3e827
Jose Velazquez <jav4534@nyu.edu>
2022-02-09 01:28:22 -0500

    Added decode to request string, added response strings for 200 and 404, added instruction to close file

--- solution.py
+++ solution.py
@@ -10,36 +10,45 @@ def webServer(port=13331):
   #Prepare a server socket
   serverSocket.bind(("", port))
   #Fill in start
-  serverSocket.listen()
+  serverSocket.listen(1)
   #Fill in end
 
   while True:
     #Establish the connection
-    #print('Ready to serve...')
+    print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()#Fill in start      #Fill in end
+    response = ""
     try:
 
       try:
-        message = "GET /helloworld.html"#Fill in start    #Fill in end
-        filename = message.split()[1]
+        request = connectionSocket.recv(1024).decode()#Fill in start    #Fill in end
+        print(request)
+        filename = request.split()[1]
+        print(filename)
         f = open(filename[1:])
-        outputdata = "200 OK"#Fill in start     #Fill in end
+        content = f.read(1024)#Fill in start     #Fill in end
         
         #Send one HTTP header line into socket.
         #Fill in start
-
+        f.close()
+        response = "HTTP/1.1 200 OK\n\n"
+        response = response + content
         #Fill in end
-
+        
         #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-          connectionSocket.send(outputdata[i].encode())
+        for i in range(0, len(response)):
+          connectionSocket.send(response[i].encode())
 
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
       except IOError:
         # Send response message for file not found (404)
         #Fill in start
-        outputdata = "404 Not found"
+        response = "HTTP/1.1 404 Not Found\n\n"
+        for i in range(0, len(response)):
+          connectionSocket.send(response[i].encode())
+
+        connectionSocket.send("\r\n".encode())
         #Fill in end
 
 
